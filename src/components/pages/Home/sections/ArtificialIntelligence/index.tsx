736de11deccfee0aab2aba30bf2dee48c2f7908b
Bruno Silva <bruno3dcontato@gmail.com>
2022-02-12 19:44:46 -0300

    chore: add tab system to ai section

--- src/components/pages/Home/sections/ArtificialIntelligence/index.tsx
+++ src/components/pages/Home/sections/ArtificialIntelligence/index.tsx
@@ -5,8 +5,8 @@ import cn from 'classnames';
 
 import { useTabState, Tab, TabList, TabPanel } from 'reakit/Tab';
 
-import { NASA_PROJECT_AWARD } from '@/data/urls';
-import { Container, Subtitle, Text } from '@/components';
+import { NASA_PROJECT_AWARD, WHITEPAPER_URL } from '@/data/urls';
+import { Container, LinkButton, Subtitle, Text } from '@/components';
 
 import styles from './styles.module.css';
 
@@ -76,9 +76,9 @@ export default function ArtificialIntelligence({
             aria-label="Artificial Intelligence Features"
           >
             <Tab {...tab} className={styles.tab} id="artist">
-              <Subtitle className={styles.subtitle} as="h5">
+              <Text className={styles.text} as="span">
                 1. Artist
-              </Subtitle>
+              </Text>
             </Tab>
 
             <div className={styles.chevron}>
@@ -91,9 +91,9 @@ export default function ArtificialIntelligence({
             </div>
 
             <Tab {...tab} className={styles.tab} id="workshop">
-              <Subtitle className={styles.subtitle} as="h5">
+              <Text className={styles.text} as="span">
                 2. Workshop
-              </Subtitle>
+              </Text>
             </Tab>
 
             <div className={styles.chevron}>
@@ -106,22 +106,98 @@ export default function ArtificialIntelligence({
             </div>
 
             <Tab {...tab} className={styles.tab} id="artwork">
-              <Subtitle className={styles.subtitle} as="h5">
+              <Text className={styles.text} as="span">
                 3. Artwork
-              </Subtitle>
+              </Text>
             </Tab>
           </TabList>
         </div>
 
-        <div className={styles.tabPanel}>
-          <TabPanel {...tab}>
-            {tab.selectedId === `artist` && `Artist`}
+        <div className={styles.tabPanelContainer}>
+          <TabPanel {...tab} className={styles.tabPanel}>
+            {tab.selectedId === `artist` && (
+              <>
+                <div className={styles.panelImage}>
+                  <Image
+                    src="/images/the-great-wave-off-kanagawa.jpg"
+                    alt="The Great Wave off Kanagawa"
+                    width="492"
+                    height="373"
+                  />
+                </div>
+                <div className={styles.tabPanelContent}>
+                  <Subtitle className={styles.subtitle} as="h5">
+                    Artist
+                  </Subtitle>
+
+                  <Text className={styles.description}>
+                    Has a unique and recognizable style that will be imprinted
+                    to the Artwork.
+                  </Text>
+
+                  <LinkButton className={styles.cta} href={WHITEPAPER_URL}>
+                    Read Whitepaper
+                  </LinkButton>
+                </div>
+              </>
+            )}
           </TabPanel>
-          <TabPanel {...tab}>
-            {tab.selectedId === `workshop` && `Workshop`}
+          <TabPanel {...tab} className={styles.tabPanel}>
+            {tab.selectedId === `workshop` && (
+              <>
+                <div className={styles.panelImage}>
+                  <Image
+                    src="/images/asteroid-vesta.jpg"
+                    alt="The Vesta 4 asteroid"
+                    width="492"
+                    height="373"
+                  />
+                </div>
+                <div className={styles.tabPanelContent}>
+                  <Subtitle className={styles.subtitle} as="h5">
+                    Workshop
+                  </Subtitle>
+
+                  <Text className={styles.description}>
+                    The location and source of inspiration for the Artist. It
+                    determines the theme of the artwork (landscape, portrait,
+                    meme, etc...).
+                  </Text>
+
+                  <LinkButton className={styles.cta} href={WHITEPAPER_URL}>
+                    Read Whitepaper
+                  </LinkButton>
+                </div>
+              </>
+            )}
           </TabPanel>
-          <TabPanel {...tab}>
-            {tab.selectedId === `artwork` && `Artwork`}
+          <TabPanel {...tab} className={styles.tabPanel}>
+            {tab.selectedId === `artwork` && (
+              <>
+                <div className={styles.panelImage}>
+                  <Image
+                    src="/images/artai-vesta-output.jpg"
+                    alt="The Great Wave off Kanagawa"
+                    width="492"
+                    height="373"
+                  />
+                </div>
+                <div className={styles.tabPanelContent}>
+                  <Subtitle className={styles.subtitle} as="h5">
+                    Artwork
+                  </Subtitle>
+
+                  <Text className={styles.description}>
+                    Has a unique and recognizable style that will be imprinted
+                    to the Artwork.
+                  </Text>
+
+                  <LinkButton className={styles.cta} href={WHITEPAPER_URL}>
+                    Read Whitepaper
+                  </LinkButton>
+                </div>
+              </>
+            )}
           </TabPanel>
         </div>
       </Container>
